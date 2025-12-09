8fd9a4f77af82e6846a300adc6c92a5de5e2df0d
Prisha Vadhavkar <prishavadhavkar@gmail.com>
2025-12-09 21:50:39 +0530

    db connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import path, { dirname } from "path"
 import authRoutes from "./routes/auth.js"
 import messageRoutes from "./routes/message.js"
+import { connectDB } from "./lib/db.js"
 
 const app = express()
 const __dirname = path.resolve(); 
@@ -17,4 +18,7 @@ if(process.env.NODE_ENV === "production"){
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => { console.log("Server running on port:" + PORT)});
+app.listen(PORT, () => { 
+    console.log("Server running on port:" + PORT)
+    connectDB();
+});
