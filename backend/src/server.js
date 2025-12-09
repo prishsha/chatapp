c89396fb21b35cf5a476ec61f05a0a696eadfd8c
Prisha Vadhavkar <prishavadhavkar@gmail.com>
2025-12-09 17:22:01 +0530

    prod env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,20 @@
-import express from "express";
+import express from "express"
 import dotenv from "dotenv"
+import path, { dirname } from "path"
 import authRoutes from "./routes/auth.js"
 import messageRoutes from "./routes/message.js"
 
 const app = express()
+const __dirname = path.resolve(); 
 dotenv.config();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+}
+
 const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => { console.log("Server running on port:" + PORT)});
+app.listen(PORT, () => { console.log("Server running on port:" + PORT)});
